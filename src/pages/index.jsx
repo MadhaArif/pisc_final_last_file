c226ee777556f943588d314c15896ae4e1643e34
Madha Arif <madhaarif9@gmail.com>
2026-05-24 00:56:43 +0500

    Cleanup unused imports and fix potential undefined module issue

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Image from "next/image";
 import HomeUniversity from "../components/homes/home-university";
 import SEO from "../components/seo";
